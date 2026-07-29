db34f4242fe827063c4c6c0025a02b231e3099a9
Gabriel Martinez <gam009@ucsd.edu>
2026-07-29 17:13:47 +0000

    updated: formatted code

--- assignment-1/homework/huffman/src/main.cpp
+++ assignment-1/homework/huffman/src/main.cpp
@@ -6,82 +6,86 @@
 
 using namespace std;
 
+int main(int argc, const char *argv[])
+{
+	cout << "WES237B Assignment 1\n";
 
-int main(int argc, const char * argv[])
-{    
-    cout << "WES237B Assignment 1\n";
-    
-	if(argc < 4)
+	if (argc < 4)
 	{
 		cout << "Usage: " << argv[0] << " <input.txt> <code.txt> <output.txt>" << endl;
 		return EXIT_FAILURE;
 	}
 
-	const char* in_filename   = argv[1];
-	const char* code_filename = argv[2];
-	const char* out_filename  = argv[3];
+	const char *in_filename = argv[1];
+	const char *code_filename = argv[2];
+	const char *out_filename = argv[3];
 
 	// Read input
 	ifstream in(in_filename);
-	
+
 	string in_contents((istreambuf_iterator<char>(in)), istreambuf_iterator<char>());
 
-
 	// Encode
-	unsigned char* code = NULL;
+	unsigned char *code = NULL;
 	uint32_t code_size = 0;
 
 	int ret;
-	ret = huffman_encode((unsigned char*)in_contents.c_str(), in_contents.size(), &code, &code_size);
+	ret = huffman_encode((unsigned char *)in_contents.c_str(), in_contents.size(), &code, &code_size);
 
-	if(ret != 0)
-	{ cerr << "Huffman encode failed with code " << ret << endl; return EXIT_FAILURE; }
-
+	if (ret != 0)
+	{
+		cerr << "Huffman encode failed with code " << ret << endl;
+		return EXIT_FAILURE;
+	}
 
 	// Save code to file
-	if(code)
+	if (code)
 	{
 		ofstream out_code(code_filename);
-		for(uint32_t i = 0; i < code_size; i++)
+		for (uint32_t i = 0; i < code_size; i++)
 		{
 			out_code << code[i];
 		}
 	}
 
-
 	// Read code from the same file
 	ifstream in_code(code_filename);
 	string code_contents((istreambuf_iterator<char>(in_code)), istreambuf_iterator<char>());
 
-
 	// Decode
-	unsigned char* decode = NULL;
+	unsigned char *decode = NULL;
 	uint32_t decode_size = 0;
 
-	ret = huffman_decode((unsigned char*)code_contents.c_str(), code_contents.size(), &decode, &decode_size);
+	ret = huffman_decode((unsigned char *)code_contents.c_str(), code_contents.size(), &decode, &decode_size);
 
-	if(ret != 0)
-	{ cerr << "Huffman decode failed with code " << ret << endl; return EXIT_FAILURE; }
-
-
-	// Save output to file
-	if(decode)
+	if (ret != 0)
 	{
-		ofstream out(out_filename);
-		out << (const char*)decode;
+		cerr << "Huffman decode failed with code " << ret << endl;
+		return EXIT_FAILURE;
 	}
 
+	// Save output to file
+	if (decode)
+	{
+		ofstream out(out_filename);
+		out << (const char *)decode;
+	}
 
 	// Free memory
-	if(code){ free(code); }
-	if(decode){ free(decode); }
-
+	if (code)
+	{
+		free(code);
+	}
+	if (decode)
+	{
+		free(decode);
+	}
 
 	// Check output
 	string diff_command = "diff " + string(in_filename) + " " + string(out_filename);
 	int diff_ret = system(diff_command.c_str());
 
-	if(WEXITSTATUS(diff_ret) == 0)
+	if (WEXITSTATUS(diff_ret) == 0)
 	{
 		cout << "SUCCESS" << endl;
 		return EXIT_SUCCESS;
@@ -91,5 +95,5 @@ int main(int argc, const char * argv[])
 		cout << "FAILURE" << endl;
 		return EXIT_FAILURE;
 	}
-    return 0;
+	return 0;
 }
